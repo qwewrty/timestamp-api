aa7020907fff7236a4e7553933c2c7caa2e19eae
qazxswedc <sandeep.carmel@gmail.com>
2016-11-17 09:19:56 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app = express();
+app.get('/', function(req,res) {
+    res.send('Hello!');
+});
+app.listen(8080, function(){
+    console.log('listening on 8080:')
+})
