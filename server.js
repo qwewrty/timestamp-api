91d30661594c07c94957a98f2e5d37ec2d8d0d9d
qazxswedc <sandeep.carmel@gmail.com>
2016-11-17 10:51:17 +0000

    Adding server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express = require("express");
 var app = express();
-app.get('/', function(req,res) {
-    res.send('Hello!');
-});
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+app.get('/',function (req,res) {
+    res.send("Hello!");
+})
+app.get('/:time', function(req,res) {
+    var date;
+    if(!isNaN(Number(req.params.time)))
+        date  = new Date(req.params.time*1000);
+    else
+        date = new Date(req.params.time);
+    //console.log(date);
+    //console.log(Number(req.params.time))
+;    var obj = {
+        unix: date.getTime(),
+        normal: months[date.getMonth()]+" "+date.getDate()+", "+date.getUTCFullYear()
+    };
+    res.send(JSON.stringify(obj));
+})
 app.listen(8080, function(){
     console.log('listening on 8080:')
 })
